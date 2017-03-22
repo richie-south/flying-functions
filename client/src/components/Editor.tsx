1d9a712a4ba4bd8927f7a8c796e50e0863ad2f21
rs222kn <rs222kn@student.lnu.se>
2017-03-22 14:26:40 +0100

    ace editor for flying function

---
+++ client/src/components/Editor.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import AceEditor from 'react-ace'
+import 'brace/mode/javascript'
+import 'brace/theme/monokai'
+import {standard} from '../template/code'
+
+import {connect} from 'react-redux'
+
+import {store} from '../lib/store'
+import {compose, lifecycle, shouldUpdate, withHandlers, withState} from 'recompose'
+import { sendFlyingFunction, saveFlyingFunction } from '../lib/action-creators/flying-function';
+
+const enhance: any = compose(
+  //connect(state => ({ flyingFunction })),
+  withHandlers({
+    handleChange: ({dispatch}) => code => saveFlyingFunction(store.dispatch, code) 
+  }),
+)
+
+type Props = {
+  handleChange: Function,
+}
+
+const _Editor = ({
+  handleChange,
+}: Props) =>
+   <AceEditor
+    mode="javascript"
+    theme="monokai"
+    height={'300px'}
+    width={'600px'}
+    showGutter={false}
+    onChange={value => handleChange(value)}
+    name="_AceEditor"
+    defaultValue={standard}
+    tabSize={2}
+    editorProps={{$blockScrolling: true} as any}
+  />
+
+export const Editor = enhance(_Editor)
