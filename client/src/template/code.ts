4b65984c230e61abac357cc98c2905350c9ccb9f
rs222kn <rs222kn@student.lnu.se>
2017-03-22 14:27:00 +0100

    code template

---
+++ client/src/template/code.ts
@@ -0,0 +1,9 @@
+export const standard = 
+`/*
+ * 
+ * @param data [express req.query (GET) or express req.data (POST)]
+ * @param currentInvocation [nr of times function has been invocated]
+ */
+module.exports = async (data, currentInvocation) => {
+  
+};`
