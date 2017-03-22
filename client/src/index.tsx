b43278cdd804f64a6079ca159d05fb9614ddc96f
rs222kn <rs222kn@student.lnu.se>
2017-03-22 14:23:56 +0100

    index

---
+++ client/src/index.tsx
@@ -0,0 +1,33 @@
+import 'babel-polyfill'
+import * as React from 'react'
+import { render } from 'react-dom'
+import { App } from './App'
+import { Provider } from 'react-redux'
+import {store} from './lib/store'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function renderApp(App) {
+  const app = document.getElementById('app')
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>, 
+    app
+  )
+}
+
+/*
+if (window.document) {
+  window['React'] = React
+  renderApp(App)
+}
+
+if (module['hot']) {
+  module['hot'].accept('./app', () => {
+    const UpdatedApp = require('./app').app
+    setTimeout(() => renderApp(UpdatedApp))
+  })
+}
+*/
+
+renderApp(App)
