06d8700f1f876b6c5da9d0852008782c9473bfcf
rs222kn <rs222kn@student.lnu.se>
2017-03-22 11:09:38 +0100

    added getByUrlId and remove

--- dal/codeStorageHandler.js
+++ dal/codeStorageHandler.js
@@ -3,11 +3,15 @@
 const CodeStorage = require('../model/CodeStorage')
 
 const create = (code, name, invocations = 0) => 
-  new CodeStorage({ code, name, invocations }).save()
+  new CodeStorage({ code, name, invocations, urlId: '00' }).save()
 
 const getById = (_id) => 
   CodeStorage.findOne({ _id })
-  .exec()
+    .exec()
+
+const getByUrlId = (urlId) => 
+  CodeStorage.findOne({ urlId })
+    .exec()
 
 const updateInvocations = (_id, invocations) => 
   CodeStorage.update({_id }, {$set: { invocations }}).exec()
@@ -15,18 +19,18 @@ const updateInvocations = (_id, invocations) =>
 const getAll = () => 
   CodeStorage.find({}).exec()
 
+const remove = (_id) =>
+  CodeStorage.findOne({ _id }).remove().exec()
 
 /*const update = (_id, title, content) =>
-  CodeStorage.update({ _id }, { title, content }).exec()
-
-const remove = (_id) =>
-  CodeStorage.findOne({ _id }).remove().exec()*/
+  CodeStorage.update({ _id }, { title, content }).exec()*/
 
 module.exports = {
   create,
   getById,
   getAll,
-  /*update,
-  remove,*/
+  /*update, */
+  getByUrlId,
+  remove,
   updateInvocations,
 }
