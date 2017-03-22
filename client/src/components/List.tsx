8bb438c6ea9f092a4d4c7102f3dadeb37f1d225b
rs222kn <rs222kn@student.lnu.se>
2017-03-22 15:51:22 +0100

    Redner list of response

---
+++ client/src/components/List.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import {store} from '../lib/store'
+import {connect} from 'react-redux'
+import { ListGroup, ListGroupItem } from 'reactstrap'
+import {compose, lifecycle, shouldUpdate, withHandlers, withState} from 'recompose'
+import { saveFlyingFunctionName } from '../lib/action-creators/flying-function-name'
+
+const enhance: any = compose(
+  connect(
+    ({
+      flyingFunctionResponse:{
+        invocationUrl,
+        secretId,
+        name,
+        urlId,
+      }
+    }) => ({
+      invocationUrl,
+      secretId,
+      name,
+      urlId
+    })
+  )
+)
+
+type Props = {
+  invocationUrl: string,
+  secretId: string,
+  name: string,
+  urlId: string
+}
+
+const _List = ({
+  invocationUrl,
+  secretId,
+  name,
+  urlId,
+}: Props) =>
+  <ListGroup>
+    <ListGroupItem>InvicationURL: <a href={invocationUrl}>{invocationUrl}</a></ListGroupItem>
+    <ListGroupItem>SecretId: {secretId}</ListGroupItem>
+    <ListGroupItem>Name: {name}</ListGroupItem>
+    <ListGroupItem>UrlId: {urlId}</ListGroupItem>
+  </ListGroup>
+
+export const List = enhance(_List)
