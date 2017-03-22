1a22c5c8afe41408963ad07949b4fcbfdf6614c4
rs222kn <rs222kn@student.lnu.se>
2017-03-22 15:51:30 +0100

    Hader content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {Editor} from './components/Editor'
 import {SaveButton} from './components/Save-button'
 import { Input } from './components/Input'
+import { List } from './components/List'
 import { Jumbotron, Container, Row, Col } from 'reactstrap'
 
 export const App = () => 
@@ -9,14 +10,17 @@ export const App = () =>
 
     <Jumbotron fluid>
       <Container fluid>
-        <h1 className="display-3">Flying function</h1>
-        <p className="lead">Enter name and flying function bellow</p>
+        <h1 className="display-3">Flying function <img src="https://raw.githubusercontent.com/richie-south/flying-functions/master/ff.png" width="60"/></h1>
+        <p className="lead">Enter name and flying function bellow <a href="https://github.com/richie-south/flying-functions">Github repo</a></p>
       </Container>
     </Jumbotron>
-    <Container> 
+    
+    <Container style={{ maxWidth: "630px"}}> 
       <Input />
       <Editor/>
       <SaveButton/>
+      <List/>
+      
     </Container>
   </div>
   
