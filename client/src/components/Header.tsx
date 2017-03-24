123044718dc504765424052dfbdf50aa74284f0a
rs222kn <rs222kn@student.lnu.se>
2017-03-24 14:53:57 +0100

    Main to header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Jumbotron, Container, Row, Col } from 'reactstrap'
+
+export const Header = () => 
+  <Jumbotron fluid>
+    <Container fluid>
+      <h1 className="display-3">Flying function <img src="https://raw.githubusercontent.com/richie-south/flying-functions/master/ff.png" width="60"/></h1>
+      <p className="lead">Enter name and flying function bellow <a href="https://github.com/richie-south/flying-functions">Github repo</a></p>
+    </Container>
+  </Jumbotron>
