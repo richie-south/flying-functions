6a9ded1abc3f94009faa73187d31b5ae0c8762c5
rs222kn <rs222kn@student.lnu.se>
2017-03-24 14:52:59 +0100

    added flying id to template

--- client/src/template/code.ts
+++ client/src/template/code.ts
@@ -3,7 +3,8 @@ export const standard =
  * 
  * @param data [express req.query (GET) or express req.data (POST)]
  * @param currentInvocation [nr of times function has been invocated]
+ * @param flyingId [uniq id for flying function, same as urlId]
  */
-module.exports = async (data, currentInvocation) => {
+module.exports = async (data, currentInvocation, flyingId) => {
   
 };`
