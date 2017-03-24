10ac9ade085019a22d943c6daad17fdd3c0b1c16
rs222kn <rs222kn@student.lnu.se>
2017-03-24 14:53:47 +0100

    Changed to react router v4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,14 @@
 import * as React from 'react'
-import {Editor} from './components/Editor'
-import {SaveButton} from './components/Save-button'
-import { Input } from './components/Input'
-import { List } from './components/List'
-import { Jumbotron, Container, Row, Col } from 'reactstrap'
+import { FlyingFunctions } from './components/pages/FlyingFunctions'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+import Route from 'react-router/Route'
+import { Header } from './components/Header'
+import Switch from 'react-router/Switch'
 
 export const App = () => 
-  <div>
-
-    <Jumbotron fluid>
-      <Container fluid>
-        <h1 className="display-3">Flying function <img src="https://raw.githubusercontent.com/richie-south/flying-functions/master/ff.png" width="60"/></h1>
-        <p className="lead">Enter name and flying function bellow <a href="https://github.com/richie-south/flying-functions">Github repo</a></p>
-      </Container>
-    </Jumbotron>
-    
-    <Container style={{ maxWidth: "630px"}}> 
-      <Input />
-      <Editor/>
-      <SaveButton/>
-      <List/>
-      
-    </Container>
-  </div>
-  
+  <BrowserRouter>
+    <div>
+      <Route component={Header}/>
+      <Route path='/flying' component={FlyingFunctions} />
+    </div>
+  </BrowserRouter>
