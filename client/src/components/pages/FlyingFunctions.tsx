d646a3817161ad00d8bb08a6c0d5baa533e077c7
rs222kn <rs222kn@student.lnu.se>
2017-03-24 23:15:41 +0100

    more routes and simple nav.. will change

--- client/src/components/pages/FlyingFunctions.tsx
+++ client/src/components/pages/FlyingFunctions.tsx
@@ -1,12 +1,31 @@
 import * as React from 'react'
 import { CreateFlyingFunction } from './CreateFlyingFunction'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
+import { Nav, NavItem, NavLink } from 'reactstrap'
+import { Container } from '../Container'
 
 export const FlyingFunctions = ({ match }) => 
-  <div>
+  <Container>
+    <Nav>
+      <NavItem>
+        <Link to={`${match.url}/create`}>
+            Create
+          </Link>
+        <NavLink href="#">
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+    </Nav>
+
+
     <Route path={`${match.url}/create`} component={CreateFlyingFunction}/>
+    <Route path={`${match.url}/view`} component={CreateFlyingFunction}/>
+    <Route path={`${match.url}/update`} component={CreateFlyingFunction}/>
+    <Route path={`${match.url}/delete`} component={CreateFlyingFunction}/>
 
     <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
+      <div>Please select what you want to do.</div>
     )}/>
-  </div>
+  </Container>
