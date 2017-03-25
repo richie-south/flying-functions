b9b3116a80f7a26f44ff13ae1340da89d954a66b
rs222kn <rs222kn@student.lnu.se>
2017-03-25 14:57:18 +0100

    Added metods

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.use('/', router)
 
 app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*')
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.header("Access-Control-Allow-Headers", 'X-Requested-With')
   res.header('Access-Control-Allow-Headers', 'Content-Type')
   next()
 })
