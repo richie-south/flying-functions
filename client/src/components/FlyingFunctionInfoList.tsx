935f1571500188bc0fb44010f344d958b5de6a11
rs222kn <rs222kn@student.lnu.se>
2017-03-26 17:39:27 +0200

    renamed and added optional props

--- client/src/components/FlyingFunctionInfoList.tsx
+++ client/src/components/FlyingFunctionInfoList.tsx
@@ -23,11 +23,15 @@ const enhance: any = compose(
   )
 )
 
-type Props = {
+export type Props = {
   invocationUrl: string,
   secretId: string,
   name: string,
-  urlId: string
+  urlId: string,
+  code?: string,
+  invocations?: number,
+  createdAt?: string,
+  updatedAt?: string,
 }
 
 export const _List = ({
@@ -35,12 +39,20 @@ export const _List = ({
   secretId,
   name,
   urlId,
+  code,
+  invocations,
+  createdAt,
+  updatedAt,
 }: Props) =>
   <ListGroup>
     <ListGroupItem>InvicationURL: <a href={invocationUrl}>{invocationUrl}</a></ListGroupItem>
     <ListGroupItem>SecretId: {secretId}</ListGroupItem>
     <ListGroupItem>UrlId: {urlId}</ListGroupItem>
     <ListGroupItem>Name: {name}</ListGroupItem>
+    {invocations !== 0 && <ListGroupItem>Invocations: {invocations === -1 ? 0 : invocations }</ListGroupItem>}
+    {createdAt && <ListGroupItem>CreatedAt: {createdAt}</ListGroupItem>}
+    {updatedAt && <ListGroupItem>UpdatedAt: {updatedAt}</ListGroupItem>}
+    {code && <ListGroupItem>Code: <pre>{code}</pre></ListGroupItem>}
   </ListGroup>
 
 export const List = enhance(_List)
