b64b4b5566b82c96a42fb1c92de8531e2f2e7cf6
rs222kn <rs222kn@student.lnu.se>
2017-03-26 17:40:57 +0200

    added view get function

--- client/src/lib/dal/flyingFunction.ts
+++ client/src/lib/dal/flyingFunction.ts
@@ -1,7 +1,7 @@
 import { get, post, doDelete } from '../http'
 
-const baseUrl = 'https://richardsoderman.se/projectflying'
-//const baseUrl = 'http://localhost:5001'
+//const baseUrl = 'https://richardsoderman.se/projectflying'
+const baseUrl = 'http://localhost:5001'
 const flying = 'flying'
 
 export const createFlyingFunction = ({ code, name }) => 
@@ -12,3 +12,6 @@ export const createFlyingFunction = ({ code, name }) =>
 
 export const deleteFlyingFunction = (id: string) => 
   doDelete(`${baseUrl}/${flying}/${id}`)
+
+export const viewFlyingFunction = (id: string) => 
+  get(`${baseUrl}/${flying}/${id}`)
