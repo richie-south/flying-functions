126a13c19d3fec0bda1a14de94974193317fa15d
rs222kn <rs222kn@student.lnu.se>
2017-03-26 17:39:34 +0200

    renamed

--- client/src/components/pages/flyingFunction/Create.tsx
+++ client/src/components/pages/flyingFunction/Create.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Editor } from '../../Editor'
 import { Button } from '../../Button'
 import { Input } from '../../Input'
-import { List } from '../../CreatedFlyingFunctionInfoList'
+import { List } from '../../FlyingFunctionInfoList'
 
 export const Create = () => 
   <div>
