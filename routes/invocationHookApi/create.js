1d7a576b7eee46a29bdf948c83cc803533b191c7
rs222kn <rs222kn@student.lnu.se>
2017-04-12 13:23:19 +0200

    fixed webhook creation

--- routes/invocationHookApi/create.js
+++ routes/invocationHookApi/create.js
@@ -6,13 +6,13 @@ const invocationHookHandler = require('../../dal/invocationHookHandler')
 const create = async (req, res) => {
   const { id } = req.params
   const { url } = req.body
-  
   try {
-    const { _id } = await invocationHookHandler.create(url, id)
-  
+    const { _id, url: _url } = await invocationHookHandler.create(id, url)
+    
     res.status(200).json({
-      message: 'hoock created!',
+      message: 'hook created!',
       id: _id,
+      url: _url,
     })
   } catch (error) {
     res.status(500).json({message: error.message})
