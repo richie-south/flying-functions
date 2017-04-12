8e84794264d83f4eae3ee56b37c490522acf29e2
rs222kn <rs222kn@student.lnu.se>
2017-04-12 13:25:28 +0200

    updated template comments

--- client/src/template/code.ts
+++ client/src/template/code.ts
@@ -3,7 +3,7 @@ export const standard =
  * 
  * @param data [express req.query (GET) or express req.data (POST)]
  * @param currentInvocation [nr of times function has been invocated]
- * @param flyingId [uniq id for flying function, same as urlId]
+ * @param flyingId [uniq id for flying function, same as uniq id in url]
  */
 module.exports = async (data, currentInvocation, flyingId) => {
   
