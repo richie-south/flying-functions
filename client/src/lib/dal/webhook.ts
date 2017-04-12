e13180a5a9e74a4e05d6cd667de194c75b015076
rs222kn <rs222kn@student.lnu.se>
2017-04-12 13:27:06 +0200

    reader webhook dal

---
+++ client/src/lib/dal/webhook.ts
@@ -0,0 +1,19 @@
+import { get, post, doDelete, put } from '../http'
+
+//const baseUrl = 'https://richardsoderman.se/projectflying'
+const baseUrl = 'http://localhost:5001'
+const webhook = 'webhook'
+
+export const createWebhook = (id: string, url: string) => 
+  post(`${baseUrl}/${webhook}/${id}`, {
+    url,
+  })
+
+export const deleteWebhook = (id: string) => 
+  doDelete(`${baseUrl}/${webhook}/${id}`)
+
+export const viewWebhook = (id: string) => 
+  get(`${baseUrl}/${webhook}/${id}`)
+
+export const updateWebhook = (id: string, code: string) => 
+  put(`${baseUrl}/${webhook}/${id}`, { id, code })
