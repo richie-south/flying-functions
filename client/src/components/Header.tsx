c3e4abb5569716a87aa92f1cdbc13012c8455370
rs222kn <rs222kn@student.lnu.se>
2017-04-13 18:33:47 +0200

    smaller header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react'
-import { Jumbotron, Container, Row, Col } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import { Nav, NavItem, Navbar, NavbarBrand } from 'reactstrap'
+import { NavLink } from './CustomLink';
 
-export const Header = () => 
-  <Jumbotron fluid>
-    <Container fluid>
-      <h1 className="display-3">Flying function <img src="https://raw.githubusercontent.com/richie-south/flying-functions/master/ff.png" width="60"/></h1>
-      <p className="lead"><a href="https://github.com/richie-south/flying-functions">Github repo</a></p>
-    </Container>
-  </Jumbotron>
+export const Header = ({match}) => 
+  <Navbar color="faded" light toggleable>
+    <NavbarBrand href="https://github.com/richie-south/flying-functions">Flying functions <img src="https://raw.githubusercontent.com/richie-south/flying-functions/master/ff.png" width="40"/></NavbarBrand>
+    <Nav className="ml-auto" navbar>
+      <NavItem>
+        <NavLink 
+          children={'Flying function'}
+          to={`${match.url}flying`}
+        />
+      </NavItem>
+      <NavItem>
+        <NavLink 
+          children={'Webhook'}
+          to={`${match.url}webhook`}
+        />
+      </NavItem>
+    </Nav>
+  </Navbar>
+
