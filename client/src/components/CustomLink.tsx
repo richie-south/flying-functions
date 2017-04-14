7089ec119b03ac5828e6a67ff914db7978eac02d
rs222kn <rs222kn@student.lnu.se>
2017-04-14 11:01:01 +0200

    dry :thumbsup:

--- client/src/components/CustomLink.tsx
+++ client/src/components/CustomLink.tsx
@@ -1,26 +1,26 @@
 import * as React from 'react'
 import { Link, Route } from 'react-router-dom'
 
-export const CustomLink = ({ activeStyle, activeClass, children, className, to, activeOnlyWhenExact }) => (
+export const onMatchRoute = ({to, activeOnlyWhenExact}, fn) => (
   <Route 
     path={to} 
     exact={activeOnlyWhenExact} 
-    children={({ match }) => (
-      <Link 
+    children={fn}/>
+)
+
+export const CustomLink = ({ activeStyle, activeClass, children, className, to, activeOnlyWhenExact }) => 
+  onMatchRoute({to, activeOnlyWhenExact}, ({match}) => 
+    <Link 
         to={to} 
         className={(match && `${activeClass} ${className}`) || className} 
         style={match && activeStyle}>
           {children}
       </Link>
-  )}/>
-)
+  )
 
-export const CustomLinkSwitch = ({ activeStyle, activeClass, children, className, to, activeOnlyWhenExact, SwitchElement }) => (
-  <Route 
-    path={to} 
-    exact={activeOnlyWhenExact} 
-    children={({ match }) => (
-      match &&
+export const CustomLinkSwitch = ({activeStyle, activeClass, children, className, to, activeOnlyWhenExact, SwitchElement}) => 
+  onMatchRoute({to, activeOnlyWhenExact}, ({match}) => 
+    match &&
       <SwitchElement
         className={(match && `${activeClass} ${className}`) || className} 
         style={match && activeStyle}>
@@ -32,10 +32,7 @@ export const CustomLinkSwitch = ({ activeStyle, activeClass, children, className
         style={match && activeStyle}>
           {children}
       </Link>
-      
-  )}/>
-)
-
+  )
 
 export const NavLink = ({children, to}) => 
   <CustomLinkSwitch
