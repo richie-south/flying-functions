f6b3ffbbc88697fd6f2253f131d61d54731cec9c
rs222kn <rs222kn@student.lnu.se>
2017-04-18 22:14:41 +0200

    prettier js fixes

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 import { Nav, NavItem, Navbar, NavbarBrand } from 'reactstrap'
 import { NavLink } from './CustomLink';
 
-export const Header = ({match}) => 
+export const Header = ({ match }) =>
   <Navbar color="faded" light toggleable>
-    <NavbarBrand href="https://github.com/richie-south/flying-functions">Flying functions <img src="https://raw.githubusercontent.com/richie-south/flying-functions/master/ff.png" width="40"/></NavbarBrand>
+    <NavbarBrand href="https://github.com/richie-south/flying-functions">Flying functions <img src="https://raw.githubusercontent.com/richie-south/flying-functions/master/ff.png" width="40" /></NavbarBrand>
     <Nav className="ml-auto" navbar>
       <NavItem>
-        <NavLink 
+        <NavLink
           children={'Flying function'}
           to={`${match.url}flying`}
         />
       </NavItem>
       <NavItem>
-        <NavLink 
+        <NavLink
           children={'Webhook'}
           to={`${match.url}webhook`}
         />
