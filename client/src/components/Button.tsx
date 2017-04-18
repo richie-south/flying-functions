0d51482a9570c77497fbf87728d912e2fc84e6dc
rs222kn <rs222kn@student.lnu.se>
2017-04-18 22:14:07 +0200

    removed state on button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react'
-import {store} from '../lib/store'
-import {connect} from 'react-redux'
-import {compose, lifecycle, shouldUpdate, withHandlers, withState} from 'recompose'
-import {sendFlyingFunction} from '../lib/action-creators/flying-function'
+import { store } from '../lib/store'
+import { connect } from 'react-redux'
+import { compose, lifecycle, shouldUpdate, withHandlers, withState } from 'recompose'
 import { Button as BButton } from 'reactstrap'
-
-
-const enhance: any = compose(
-  withHandlers({
-    handleClick: ({dispatch}) => () => sendFlyingFunction(store.dispatch) 
-  }),
-)
 
 type Props = {
   handleClick: Function,
@@ -21,10 +13,8 @@ export const _Button = ({
   handleClick,
   name,
 }: Props) =>
-   <BButton outline color="primary"
+  <BButton outline color="primary"
     onClick={() => handleClick()}
-   >
+  >
     {name}
   </BButton>
-
-export const Button = enhance(_Button)
