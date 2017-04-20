84ce234ad56ebf692aa9497154a2a6ee39d20123
rs222kn <rs222kn@student.lnu.se>
2017-04-20 22:52:31 +0200

    prettier fixes

--- routes/flyingFunctionsApi/remove.js
+++ routes/flyingFunctionsApi/remove.js
@@ -5,26 +5,25 @@ const mongooseErrors = require('../../lib/customErrors/mongooseErrors')
 
 const remove = async (req, res) => {
   const { id } = req.params
-  
+
   try {
-    
     await codeStorageHandler.remove(id)
 
     res.status(200).json({
       message: 'Flying function removed',
     })
   } catch (error) {
-    if(mongooseErrors.isValidatorError(error)){
+    if (mongooseErrors.isValidatorError(error)) {
       return res.status(400).json({
         message: error.errors.value.message,
       })
-    }else if(mongooseErrors.isCastError(error)){
+    } else if (mongooseErrors.isCastError(error)) {
       return res.status(400).json({
         message: error.message,
       })
     }
-    res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message })
   }
 }
 
-module.exports = remove
+module.exports = remove;
