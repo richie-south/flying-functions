38e4edb3bf5b32ffbf6fdc200f5ea0007b1afaab
rs222kn <rs222kn@student.lnu.se>
2017-04-23 15:57:10 +0200

    moved store provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,16 @@ import { HashRouter } from 'react-router-dom'
 import Route from 'react-router/Route'
 import { Header } from './components/Header'
 import Switch from 'react-router/Switch'
+import { Provider } from 'react-redux'
+import {store} from './lib/store'
 
 export const App = () =>
-  <HashRouter>
-    <div>
-      <Route component={Header} />
-      <Route path='/flying' component={FlyingFunctions} />
-      <Route path='/webhook' component={Webhooks} />
-    </div>
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <div>
+        <Route component={Header} />
+        <Route path='/flying' component={FlyingFunctions} />
+        <Route path='/webhook' component={Webhooks} />
+      </div>
+    </HashRouter>
+  </Provider>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,17 +2,14 @@ import 'babel-polyfill'
 import * as React from 'react'
 import { render } from 'react-dom'
 import { App } from './App'
-import { Provider } from 'react-redux'
-import {store} from './lib/store'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import './scss/header.scss'
 
 function renderApp(App) {
   const app = document.getElementById('app')
   render(
-    <Provider store={store}>
-      <App />
-    </Provider>, 
+      <App />,
     app
   )
 }
