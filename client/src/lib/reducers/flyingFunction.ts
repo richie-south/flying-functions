d92e05459c6c9dced8a37ae93c5ac25ea5c426a7
rs222kn <rs222kn@student.lnu.se>
2017-04-24 21:27:12 +0200

    remove and store implemented

--- client/src/lib/reducers/flyingFunction.ts
+++ client/src/lib/reducers/flyingFunction.ts
@@ -3,5 +3,8 @@ import {createReducer} from 'redux-decorated'
 import { actions} from '../actions'
 
 export const flyingFunctions = createReducer([])
-  .when(actions.flyingFunctions, (state, flyingFunction) => [...state, flyingFunction])
+  .when(actions.storeFlyingFunction, (state, flyingFunction) => [...state, flyingFunction])
+  .when(actions.removeFlyingFunction, (state, id) => 
+    state.filter(fF => fF.secretId === id ? false : true)
+  )
   .build()
