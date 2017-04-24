be7d40789b652f614741a0353d7a6d93317135d9
rs222kn <rs222kn@student.lnu.se>
2017-04-24 12:50:02 +0200

    removed params

--- client/src/template/code.ts
+++ client/src/template/code.ts
@@ -2,9 +2,7 @@ export const standard =
 `/*
  * 
  * @param data [express req.query (GET) or express req.data (POST)]
- * @param currentInvocation [nr of times function has been invocated]
- * @param flyingId [uniq id for flying function, same as uniq id in url]
  */
-module.exports = async (data, currentInvocation, flyingId) => {
+module.exports = async (data) => {
   
 };`
