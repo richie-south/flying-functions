322314175726bc04f863ba9020e3247fb0abb381
rs222kn <rs222kn@student.lnu.se>
2017-04-24 21:24:13 +0200

    removed with

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -2,6 +2,6 @@ import * as React from 'react'
 import { Container as BootContainer } from 'reactstrap'
 
 export const Container : React.StatelessComponent<{}> = ({children}) =>
-	<BootContainer style={{ maxWidth: "630px"}}>
+	<BootContainer>
 		{children}
 	</BootContainer>
