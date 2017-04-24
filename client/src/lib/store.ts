c72ac125e1119e4d09e38b124a0b90d398eae3da
rs222kn <rs222kn@student.lnu.se>
2017-04-24 21:51:59 +0200

    added presist state with localstorage

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -1,6 +1,7 @@
 import { Store as ReduxStore, applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { flyingFunctions } from './reducers/flyingFunction'
+import persistState from 'redux-localstorage'
 
 const reducer = combineReducers({
   flyingFunctions,
@@ -9,6 +10,7 @@ const reducer = combineReducers({
 export const store = createStore(
   reducer,
   compose(
-    applyMiddleware(...[thunk])
+    applyMiddleware(...[thunk]),
+    persistState()
   )
 )
