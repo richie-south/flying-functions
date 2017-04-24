2281f9f58f28ff46808a4c46647136a876d1294e
rs222kn <rs222kn@student.lnu.se>
2017-04-24 21:26:59 +0200

    remove, and type implemented

--- client/src/lib/action-creators/flyingFunction.ts
+++ client/src/lib/action-creators/flyingFunction.ts
@@ -1,4 +1,18 @@
 import {action, actions} from '../actions'
 
-export const storeFlyingFunction = (dispatch, flyingFunction: {}) =>
-  dispatch(action(actions.flyingFunctions, flyingFunction))
+export type FlyingFunctionObj = {
+  code: string,
+  originalCode: string
+  name: string,
+  HTTPType: string,
+  secretId: string,
+  urlId: string,
+  invocationUrl: string,
+  message: string,
+}
+
+export const storeFlyingFunction = (dispatch, flyingFunction: FlyingFunctionObj): void =>
+  dispatch(action(actions.storeFlyingFunction, flyingFunction))
+
+export const removeFlyingFunction = (dispatch, id: string): void => 
+  dispatch(action(actions.removeFlyingFunction, id))
